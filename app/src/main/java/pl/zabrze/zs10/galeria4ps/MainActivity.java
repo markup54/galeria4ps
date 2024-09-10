38e8d966aeca9183eb355291391de4a89bf79f0f
markup54 <marzenakupka@tlen.pl>
2024-09-10 15:12:46 +0200

    zmiana rating

--- app/src/main/java/pl/zabrze/zs10/galeria4ps/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/galeria4ps/MainActivity.java
@@ -3,6 +3,7 @@ package pl.zabrze.zs10.galeria4ps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     TextView textView;
     ArrayList<Obraz> obrazki = new ArrayList<>();
+    int aktualnyIndeks =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +29,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ratingBarSrednia = findViewById(R.id.ratingBar);
         ratingBarUzytkownik = findViewById(R.id.ratingBar2);
-        buttonWstecz = findViewById(R.id.button);
-        buttonDalej = findViewById(R.id.button2);
-        buttonOcen = findViewById(R.id.button3);
+        buttonWstecz = findViewById(R.id.button2);
+        buttonDalej = findViewById(R.id.button3);
+        buttonOcen = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textViewOpis);
         obrazki.add(new Obraz(R.drawable.k2,"Szczyt K2"));
         obrazki.add(new Obraz(R.drawable.anna, "Szczyt Anna Purna"));
         obrazki.add(new Obraz(R.drawable.mont,"Szczyt Mont Everest"));
+        buttonDalej.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        aktualnyIndeks++;
+                        if(aktualnyIndeks >=obrazki.size())
+                            aktualnyIndeks =0;
+                        wyswietlObraz(aktualnyIndeks);
+                    }
+                }
+        );
+        buttonWstecz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        aktualnyIndeks--;
+                        if(aktualnyIndeks<0)
+                            aktualnyIndeks = obrazki.size()-1;
+                        wyswietlObraz(aktualnyIndeks);
+                    }
+                }
+        );
 
+        buttonOcen.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        float ocenka = ratingBarUzytkownik.getRating();
+                        obrazki.get(aktualnyIndeks).setOceny(ocenka);
+                        wyswietlObraz(aktualnyIndeks);
+                    }
+                }
+        );
 
+    }
+    private void wyswietlObraz(int x){
+        imageView.setImageResource(obrazki.get(x).getIdObrazka());
+        textView.setText(obrazki.get(x).getOpis());
+        ratingBarSrednia.setRating((float)obrazki.get(x).obliczSredniaOcen());
+        ratingBarUzytkownik.setRating(0);
     }
 }
